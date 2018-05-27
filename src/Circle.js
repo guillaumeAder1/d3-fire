b518cb038d621aedba3ef1869d306e842cdd1620
Guillaume Ader <GAder@esriuk.com>
2018-05-27 10:26:52 +0100

    refactor class and overide circle class from extened constructor Flame

--- src/Circle.js
+++ src/Circle.js
@@ -4,38 +4,47 @@ import random from 'lodash/random';
 class Circle {
     constructor(params) {
         const { x, y, node } = params;
-        this.variable = 40;
+        this.variable = 0;
         this.color = '#fff'
         this.destColor = '#fff'
+        this.origX = 0
+        this.node = params.node || 'dom'
+        this.y = params.y || 0
+        this.r = 0
+        this.transition = null
+        this.duration = 0
+        this.delay = 0
+        this.destX = 0
+        this.destY = 0
 
     }
 
-    init(d) {
+    init() {
 
         // draw circle 
-        this.circle = d.node
+        this.circle = this.node
             .append('circle')
-            .attr("cx", d.x)
-            .attr("cy", d.y)
+            .attr("cx", this.origX)
+            .attr("cy", this.y)
             .attr("r", 0)
             .style("fill", this.color)
             .style("opacity", 0)
             .transition()
             .duration(500)
-            .attr("r", d.r)
+            .attr("r", this.r)
             .style("opacity", random(.4, .9))
 
 
         // animate go up
         this.circle.transition()
-            .ease(d.transition)
-            .duration(d.duration)
-            .delay(d.delay)
-            .attr("cy", random(100, 150))
-            .attr("cx", d.destX)
+            .ease(this.transition)
+            .duration(this.duration)
+            .delay(this.delay)
+            .attr("cy", this.destY)
+            .attr("cx", this.destX)
             .style('fill', this.destColor)
             .style("opacity", random(0, .1))
-            .attr("r", d.r / 4)
+            .attr("r", this.r / 4)
             .on('end', (d, i, nodes) => {
                 d3.select(nodes[i]).remove();
             });
--- src/Flame.js
+++ src/Flame.js
@@ -10,31 +10,27 @@ class Flame extends Circle {
     constructor(params) {
         super(params);
 
-        // random colro
+        // random color
         // const listColor = ['rgb(253, 175, 31)', 'rgb(247, 141, 3)', 'orange']
         // this.destColor = listColor[random(2)]
         // this.color = listColor[random(2)]
 
-        this.destColor = 'rgb(253, 175, 31)';
-        this.color = 'orange';
-
-        this.variable = 40;
-
         const { x, y, node } = params;
-        const origX = random(x - this.variable, x + this.variable);
 
+        this.destColor = 'rgb(253, 175, 31)'; // end color
+        this.color = 'orange'; // start color
+        this.variable = 40; // 
+        this.node = node // svg dom node
+        this.y = y // start posy (2/3 of the screen)
+        this.r = random(20, 70)
+        this.transition = easeLinear
+        this.duration = random(1500, 1700)
+        this.delay = random(5)
+        this.origX = random(x - this.variable, x + this.variable);
+        this.destX = random(x - this.variable / 3, x + this.variable / 3) // destx to be centered
+        this.destY = random(y - 250, y - 350); // end y position 
 
-        const data = {
-            node: node,
-            x: origX,
-            y: y,
-            r: random(20, 70),
-            delay: random(5),
-            duration: random(1500, 1700),
-            destX: random(x - this.variable / 3, x + this.variable / 3),
-            transition: easeLinear
-        }
-        this.init(data);
+        this.init();
     }
 }
 
