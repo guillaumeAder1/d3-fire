a6074b27a0f14695c80a1df33b08efc2c2179262
Guillaume Ader <GAder@esriuk.com>
2018-05-27 16:49:48 +0100

    run push/build from npm

--- git-push.bat
+++ git-push.bat
@@ -1,9 +1,7 @@
 cd C:\Users\gader\Documents\_projects\private\d3-fire
 call npm run build
-pause
-set /P comment=Enter comment here: 
+set /P comment=Enter commit comment here: 
 echo %comment%
-pause
 git add .
 git commit -m "%comment%"
 git push origin master
