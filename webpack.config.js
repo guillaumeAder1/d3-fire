d9c44a5c2ffb03a5d5c96626828cdea940788274
Guillaume Ader <GAder@esriuk.com>
2018-05-27 17:20:50 +0100

    remove bundle analyzer from build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 
